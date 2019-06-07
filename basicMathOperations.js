d8555677b497deb11a5c83e08ffb069dd5af0c66
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-06-06 19:34:22 -0600

    Solved problem 2

--- basicMathOperations.js
+++ basicMathOperations.js
@@ -7,3 +7,38 @@ The function should return result of numbers after applying the chosen operation
 */
 
 const basicOp = (operation, value1, value2) => eval(value1 + operation + value2);
+
+/*
+
+2. Given the triangle of consecutive odd numbers:
+            1
+         3     5
+      7     9    11
+  13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the row sums of this triangle from the row index (starting at index 1)
+
+*/
+
+//My solution:
+
+const rowSumOddNumbers = rowNum => {
+  let triangle = [];
+  let oddInts = [-1];
+  for(var i=0;i<=rowNum;i++) {
+    let row = [];
+    for(var j=1;j<=i;j++) {
+      const int = oddInts[oddInts.length - 1] + 2;
+      oddInts.push(int);
+      row.push(int);
+    };
+    triangle.push(row);
+  };
+  return triangle[rowNum].reduce((accumulator, currentValue) => accumulator + currentValue);
+};
+
+//Best solution:
+
+const rowSumOddNumbers = n => Math.pow(n, 3);
