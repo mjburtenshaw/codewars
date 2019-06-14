28f2b278f7db83d759ce56e994c8182f90c52d87
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-06-14 16:06:57 -0600

    Solved problem 7

--- basicMathOperations.js
+++ basicMathOperations.js
@@ -109,3 +109,19 @@ function oddOrEven(array) {
 function oddOrEven(arr) {
   return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
 };
+
+/*
+
+7.Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+*/
+
+//My solution
+const minMax = arr => [Math.min.apply(Math, arr), Math.max.apply(Math, arr)];
+
+//Best solution
+const minMax = arr => [Math.min(Math, arr), Math.max(Math, arr)];
