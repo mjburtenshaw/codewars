c4d97113385db18e842fab5fb0c737dd1e68da4d
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-06-17 17:17:56 -0600

    Solved problem 9.

--- basicMathOperations.js
+++ basicMathOperations.js
@@ -147,3 +147,43 @@ function findShort(s){
 function findShort(s){
   return Math.min.apply(null, s.split(' ').map(w => w.length));
 }
+
+/*
+
+9. You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block in a direction and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+*/
+
+//My solution
+function isValidWalk(walk) {
+  let latitude = 0;
+  let longitude = 0;
+  for (let step in walk) {
+    switch(walk[step]){
+      case "n":
+        longitude++;
+        break;
+      case "s":
+        longitude--;
+        break;
+      case "e":
+        latitude--;
+        break;
+      case "w":
+        longitude++;
+        break;
+      default:
+        /* do nothing */
+    };
+  };
+  if(walk.length === 10) {
+    return latitude === 0 && longitude === 0;
+  } else {
+    return false;
+  };
+};
+
+//Best solution
+//Unknown, kata test cases were coded incorrectly.
