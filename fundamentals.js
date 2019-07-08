eabe20379384990857484694c681b69f590ec34e
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2019-07-08 15:10:25 -0600

    Solved problem 13

--- fundamentals.js
+++ fundamentals.js
@@ -322,3 +322,27 @@ DNAStrand.pairs = {
   C: 'G',
   G: 'C',
 }
+
+/*
+
+14. The main idea is to count all the occuring characters(UTF-8) in string. If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+What if the string is empty ? Then the result should be empty object literal { }
+
+*/
+
+//My solution
+const count = string => {  
+  let result = {};
+  string = string.split("");
+  for (let i in string) result[string[i]] ? result[string[i]]++ : result[string[i]] = 1;
+  return result;
+};
+
+//Best solution
+function count (string) {  
+  var count = {};
+  string.split('').forEach(function(s) {
+     count[s] ? count[s]++ : count[s] = 1;
+  });
+  return count;
+};
