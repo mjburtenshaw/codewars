d4ea5325d1f909f71db638f01e9516daea801374
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-07-15 08:31:53 -0600

    Solved problem 19.

--- fundamentals.js
+++ fundamentals.js
@@ -505,3 +505,20 @@ function vowelIndices(word) {
   }
   return arr;
 }
+
+/*
+
+19. Your task is to write function findSum.
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+For example:
+findSum(5) should return 8 (3 + 5)
+findSum 10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+*/
+
+//My solution & Best solution
+const findSum = n => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) if (i % 3 === 0 || i % 5 === 0) sum += i;
+  return sum;
+};
