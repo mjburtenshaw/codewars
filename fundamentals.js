987ea668eae2b7bf3b50ce9cab529fbf27ee0e36
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-07-17 08:42:47 -0600

    Solved problem 22.

--- fundamentals.js
+++ fundamentals.js
@@ -591,3 +591,32 @@ function comp(array1, array2) {
   array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
   return array1.map(v => v * v).every((v, i) => v == array2[i]);
 }
+
+/*
+
+22. A stream of data is received and needs to be reversed.
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+*/
+
+// My solution & best solution
+const dataReverse = data => {
+  let result = [];
+  for (let i = 0; i < data.length - 1; i += 8) {
+    const bit = data.slice(i, i + 8);
+    result.unshift(...bit);
+  };
+  return result;
+};
