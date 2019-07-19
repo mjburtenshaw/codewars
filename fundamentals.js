f6b085e9665137e3a5ae0fe7e0dd2338344ff531
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-07-19 07:50:11 -0600

    Solved 24: Replace With Alphabet Position.

--- fundamentals.js
+++ fundamentals.js
@@ -654,3 +654,42 @@ const longestPalindrome = str => {
     return maxPalindromeLength;
   };
 };
+
+/*
+
+24. Replace With Alphabet Position
+
+Welcome.
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+*/
+
+// My solution
+const alphabetIndex = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const alphabetPosition = text => {
+  return text.split("").map((letter, lIndex) => {
+    letter = letter.toLowerCase();
+    const alphaCharacter = /[a-z]/;
+    if (alphaCharacter.test(letter)) {
+      let index = alphabetIndex.findIndex(indexedLetter => indexedLetter === letter);
+      return index + 1 + " ";
+    };
+  }).join("").trim();
+}
+
+// Best solution
+function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map( (c) => c.charCodeAt() - 64)
+    .join(' ');
+}
