10827ae1847c64dfaf2607d2a19c6daef94d71a4
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-08-01 09:11:06 -0600

    Solved problem 33. Consecutive Strings.

--- fundamentals.js
+++ fundamentals.js
@@ -1085,3 +1085,35 @@ const findEvenIndex = arr => {
   };
   return result;
 };
+
+/*
+
+33. Consecutive strings
+
+You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Example:
+
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+Note
+
+consecutive strings : follow one after another without an interruption
+
+*/
+
+// My solution
+const longestConsec = (strarr, k) => {
+  if (k <= 0 || k > strarr.length || strarr.length === 0) return "";
+  else if (k === strarr.length) return strarr.join(""); 
+  else {
+    let longestConsecStr = strarr.slice(0, k).join("");
+    for (let i = 0; i < strarr.length - (k - 1); i++) {
+      const thisStr = strarr.slice(i, k + i).join("");
+      if (thisStr.length > longestConsecStr.length) longestConsecStr = thisStr;
+    };
+    return longestConsecStr;
+  };
+};
