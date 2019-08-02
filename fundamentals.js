f1cfd0a0c3601d6dc3c076078a9a9bb4a9134280
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-08-02 09:25:31 -0600

    Solved problem 34: Give me a Diamond

--- fundamentals.js
+++ fundamentals.js
@@ -1117,3 +1117,51 @@ const longestConsec = (strarr, k) => {
     return longestConsecStr;
   };
 };
+
+/*
+
+34. Give me a Diamond
+
+Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+Task
+
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+Examples
+
+A size 3 diamond:
+
+ *
+***
+ *
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+...that is: " *\n ***\n*****\n ***\n *\n"
+
+*/
+
+// My solution
+const diamond = n => {
+  const median = ((n - 1) / 2) + 1;
+  let gem = [];
+  if (n % 2 === 0 || n < 1) return null;
+  for (let i = median; i >= 1; i--) {
+    const leftEdge = new Array(median - i).fill(" ").join("");
+    const crystal = new Array((i * 2) - 1).fill("*").join("");
+    const rightEdge = "\n";
+    const halfKarat = leftEdge + crystal + rightEdge;
+    gem.push(halfKarat);
+    if (i < median) gem.unshift(halfKarat);
+  };
+  return gem.join("");
+};
