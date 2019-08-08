ecc3590e9df6ec299826d6d6043413104dcd689e
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2019-08-08 09:05:19 -0600

    Solved problem 38: Unwanted dollars.

--- fundamentals.js
+++ fundamentals.js
@@ -1294,3 +1294,39 @@ function decipherThis(str) {
     )
     .join(" ")
 }
+
+/*
+
+38. Unwanted dollars
+
+If you're faced with an input box, like this:
+
+                                           +--------------+
+  Enter the price of the item, in dollars: |              |
+                                           +--------------+
+do you put the $ sign in, or not? Inevitably, some people will type a $ sign and others will leave it out. The instructions could be made clearer - but that won't help those annoying people who never read instructions anyway.
+
+A better solution is to write code that can handle the input whether it includes a $ sign or not.
+
+So, we need a simple function that converts a string representing a number (possibly with a $ sign in front of it) into the number itself.
+
+Remember to consider negative numbers (the - sign may come either before or after the $ sign, if there is one), and any extraneous space characters (leading, trailing, or around the $ sign) that the users might put in. You do not need to handle trailing characters other than spaces (e.g. "1.2 3"). If the given string does not represent a number, (either with or without a $ sign), return 0.0 .
+*/
+
+/* My solution */
+/* One-liner */ 
+// const money_value = str => !isNaN(Number.parseFloat(str.replace(/[\$ ]/g, ""))) ? Number.parseFloat(str.replace(/[\$ ]/g, "")) : 0.0;
+
+/* Readable */
+const money_value = str => {
+  const dollarsAndSpaces = /[\$ ]/g;
+  const withNothing = "";
+  const cleanStr = str.replace(dollarsAndSpaces, withNothing);
+  const num = Number.parseFloat(cleanStr);
+  const numIsANumber = !isNaN(num);
+  if (numIsANumber) return num;
+  else return 0.0;
+};
+
+/* Best practice */
+const money_value = s => +s.replace(/\s|\$/g, '') || 0;
